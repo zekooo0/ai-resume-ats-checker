ceb72aa6bd319bae4f759b4db84c5855b7572a44
zekooo0 <ahmedzakria102@gmail.com>
2025-10-19 23:48:40 +0300

    fix layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
-import InterworkyWidget from '@/components/InterworkyWidget';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -28,7 +27,6 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <InterworkyWidget />
         {children}
       </body>
     </html>
