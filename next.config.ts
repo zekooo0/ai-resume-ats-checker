d0a3bb78e5a57c54787d0c93dc0d704486841dd9
zekooo0 <ahmedzakria102@gmail.com>
2025-12-03 01:10:54 +0200

    productionBrowserSourceMaps

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  productionBrowserSourceMaps: true,
 };
 
 export default nextConfig;
